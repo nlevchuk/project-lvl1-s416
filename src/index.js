7a60a02edce7f4b5b9989172324811e8987c1a30
Nikita Levchuk <nikitalevchuk@gmail.com>
2019-01-14 13:30:43 +0700

    setup eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
-/* eslint-disable no-console */
 const outputMessage = (message) => {
   console.log(message);
 };
-/* eslint-enable no-console */
 
 const start = () => {
   outputMessage('Welcome to the Brain Games!');
