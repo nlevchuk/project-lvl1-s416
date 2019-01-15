ec8a455c8387cba2444a4ffabf64ca7d1dcd3900
Nikita Levchuk <nikitalevchuk@gmail.com>
2019-01-15 23:53:24 +0700

    fix naming and big output string; replace while => for

--- src/even-game.js
+++ src/even-game.js
@@ -1,5 +1,13 @@
 import readlineSync from 'readline-sync';
 
+const isEven = num => num % 2 === 0;
+
+const getQuestionAndAnswer = () => {
+  const question = Math.floor(Math.random() * 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return { question, correctAnswer };
+};
+
 const evenGame = () => {
   console.log('\nWelcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".');
@@ -7,25 +15,24 @@ const evenGame = () => {
   const userName = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${userName}!\n`);
 
-  let questionsCount = 3;
+  const questionsCount = 3;
 
-  while (questionsCount > 0) {
-    const digit = Math.floor(Math.random() * 100);
-    const correctAnswer = (digit % 2 === 0) ? 'yes' : 'no';
+  for (let i = 0; i < questionsCount; i += 1) {
+    const { question, correctAnswer } = getQuestionAndAnswer();
 
-    console.log(`Question: ${digit}`);
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer !== correctAnswer) {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);
-      break;
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
 
     console.log('Correct!');
-    questionsCount -= 1;
   }
 
-  if (questionsCount === 0) console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default evenGame;
